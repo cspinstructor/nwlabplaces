34ae80bba7fc4c3190c8390cb0931ca7c73e1148
student <paulchin20@yahoo.com>
2018-06-26 13:00:03 +0800

    added mongodb

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 const server = express();
+const path = require('path');
+const filemgr = require('./filemgr');
 
 const port = process.env.PORT || 3000;
 
@@ -24,6 +26,8 @@ hbs.registerHelper('list', (items, options) => {
 
   return out;
 });
+
+server.use(express.static(path.join(__dirname, 'public')));
 
 server.get('/', (req, res) => {
   res.render('home.hbs');
@@ -54,12 +58,28 @@ server.post('/getplaces', (req, res) => {
 
     filteredResults = extractData(response.data.results);
 
+    filemgr.saveData(filteredResults).then((result) => {
+      res.render('result.hbs');
+    }).catch((errorMessage) => {
+      console.log(errorMessage);
+    });
+
     //res.status(200).send(filteredResults);
-    res.render('result.hbs');
+
   }).catch((error) => {
     console.log(error);
   });
 
+});
+
+
+server.get('/historical', (req, res) => {
+  filemgr.getAllData().then((result) => {
+    filteredResults = result;
+    res.render('historical.hbs');
+  }).catch((errorMessage) => {
+    console.log(errorMessage);
+  });
 });
 
 const extractData = (originalResults) => {
@@ -83,7 +103,7 @@ const extractData = (originalResults) => {
       tempObj = {
         name: originalResults[i].name,
         address: originalResults[i].vicinity,
-        photo_reference: 'http://www.51allout.co.uk/wp-content/uploads/2012/02/Image-not-found.gif',
+        photo_reference: '/no_image_found.png',
       }
     }
 
