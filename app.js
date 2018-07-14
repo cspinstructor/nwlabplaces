f5c2c679c5384b31aee5a6efaeaf8c03f109fe82
student <paulchin20@yahoo.com>
2018-07-14 14:02:26 +0800

    heroku deploy

--- app.js
+++ app.js
@@ -16,6 +16,14 @@ hbs.registerPartials(__dirname + '/views/partials');
 const PLACES_API_KEY = 'AIzaSyCsDrx0q_PSYYBIx3xrKT0p0G53ETRG2OQ';
 var filteredResults;
 
+
+if (process.env.NODE_ENV === 'production') {
+  server.use(express.static('client/build'));
+  server.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 hbs.registerHelper('list', (items, options) => {
   items = filteredResults;
   var out = "<tr><th>Name</th><th>Address</th><th>Photo</th></tr>";
